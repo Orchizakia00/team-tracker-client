606bd81641c3aec9d7aee6d3b017dd169a3824d7
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-24 03:05:22 +0600

    testimonial section added

---
+++ src/Components/Shared/SectionTitle.jsx
@@ -0,0 +1,12 @@
+
+
+const SectionTitle = ({ heading, subHeading }) => {
+    return (
+        <div className="mx-auto md:w-4/12 text-center my-8">
+            <h3 className="text-4xl font-bold border-b-2 py-4">{heading}</h3>
+            <p className="text-blue-600 font-semibold my-2">{subHeading}</p>
+        </div>
+    );
+};
+
+export default SectionTitle;
---
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,51 @@
+import SectionTitle from "../../../Components/Shared/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import { Scrollbar } from 'swiper/modules';
+import { useEffect, useState } from "react";
+import { FaQuoteLeft } from "react-icons/fa";
+
+const Testimonial = () => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+                console.log(data);
+            })
+    }, [])
+
+    return (
+        <div>
+            <SectionTitle heading={'Testimonials'} subHeading={'Our Employees Say'}></SectionTitle>
+
+            <Swiper
+                scrollbar={{
+                    hide: true,
+                }}
+                modules={[Scrollbar]}
+                className="mySwiper"
+            >
+                {
+                    reviews.map(review => <SwiperSlide key={review.id}>
+                        <div className="mx-24 my-16 flex flex-col items-center">
+
+                            <FaQuoteLeft className="mt-8 text-6xl" />
+
+                            <p className="my-8"> {review.testimonial_text} </p>
+                            <h3 className="text-2xl text-blue-500">{review.employee_name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
+                {/* <SwiperSlide>slider 1</SwiperSlide> */}
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Testimonial;
