1d9ef71e7d7e98ce02b31fa485fb08eceb420461
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-25 02:53:46 +0600

    jwt implemented but not getting the token

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,7 +5,14 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
-    
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token');
+        console.log('interceptors stopped request', token);
+        return config;
+    },function(err) {
+        return Promise.reject(err);
+    })
+
     return axiosSecure;
 };
 
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,15 +1,17 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config'
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -41,12 +43,26 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+            if (currentUser) {
+                // get token
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('token', res.data.token);
+                        }
+                    })
+            }
+            else {
+                // remove token
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         });
         return () => {
             return unsubscribe();
         }
-    }, [])
+    }, [axiosPublic])
 
     const authInfo = {
         user,
